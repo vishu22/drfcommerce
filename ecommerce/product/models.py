6092586c6182a98b882547ef5ce75a44db9fc768
= <vishal.tandon568@gmail.com>
2023-04-13 11:30:50 +0530

    created brand, category and product tables, and installed django-mptt package to create a nested category model (ex: category and subcategory), all tables have been successfully migrated

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
 
 # Create your models here.
+class Category(MPTTModel):
+    name = models.CharField(max_length=20)
+    parent = TreeForeignKey(
+        "self", on_delete=models.PROTECT, null=True, blank=True
+    )  # managing subcategory
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    digital = models.BooleanField()
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey("Category",blank=True,null=True, on_delete=models.SET_NULL)
+    def __str__(self) -> str:
+        return self.name
